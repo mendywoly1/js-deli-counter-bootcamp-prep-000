68913355f6546997bef30b72951bcb4312bc52e3
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 11:39:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine) {
   }else{
      var line = "The line is currently: "
     for(var i = 0; i < katzDeliLine.length; i++){
-       line = line + `, ${i+1}. ${katzDeliLine[i]}`
+       line = line + `${i+1}. ${katzDeliLine[i]}`
     }
     return line
   }
