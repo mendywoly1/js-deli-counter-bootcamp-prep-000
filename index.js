81069800e86a762906de4f73903dc38e5db35768
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 11:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function currentLine(katzDeliLine) {
   if(katzDeliLine.length == 0){
     return "The line is currently empty."
   }else{
-      line = ""
+      line = "The line is currently"
     for(var i = 0; i > katzDeliLine.length; i++){
        line.push(`The line is currently ${i+1} ${katzDeliLine[i]}`)
     }
