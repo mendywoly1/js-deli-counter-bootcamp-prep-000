edd1ebaacf3efb3fb750ddc354421f34af64e1aa
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 11:14:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine, name) {
-  return katzDeliLine.push(name)
+   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`
 }
