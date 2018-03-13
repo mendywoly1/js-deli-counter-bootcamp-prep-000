1625fd79cd01dcc1eda82a5dd540cf339a110260
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 11:34:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function currentLine(katzDeliLine) {
   if(katzDeliLine.length == 0){
     return "The line is currently empty."
   }else{
-     var line = "The line is currently"
+     var line = "The line is currently: "
     for(var i = 0; i < katzDeliLine.length; i++){
        line = line + `${i+1}. ${katzDeliLine[i]}`
     }
